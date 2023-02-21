6da2a8bd227bc7846189367579463d723f5b4e3d
Banjihopes <banjihopes@gmail.com>
2023-02-21 18:14:56 +0100

    commit 000

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
 const multer = require("multer")
 const path = require("path")
+const cors = require('cors');
 
 dotenv.config();
+app.use(cors({
+    origin: "https://growthpal.onrender.com"
+}));
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
